da079d52b14dbd2dc2ee54dee01aee32f0eb5d95
Gautam-Hegde <gautamatjob@protonmail.com>
2024-04-11 23:52:17 +0530

    chore: tidy code

--- src/ValidationResponse.tsx
+++ src/ValidationResponse.tsx
@@ -24,9 +24,7 @@ interface ValidationResponseProps {
 }
 
 function ValidationResponse({ response }: ValidationResponseProps) {
-  const [showValidEmails, setShowValidEmails] = useState(false);
-  const [showUnknownEmails, setShowUnknownEmails] = useState(false);
-  const [showInvalidEmails, setShowInvalidEmails] = useState(false);
+  const [openCollapse, setOpenCollapse] = useState<number | null>(null);
 
   const validEmails = response.filter((result) => result.reachable === "yes");
   const unknownEmails = response.filter(
@@ -36,117 +34,138 @@ function ValidationResponse({ response }: ValidationResponseProps) {
     (result) => !result.syntax.valid && result.reachable !== "unknown"
   );
 
+  const toggleCollapse = (index: number) => {
+    setOpenCollapse(openCollapse === index ? null : index);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="bg-white text-zinc-700 rounded-lg shadow-md p-6 w-full max-w-lg">
-        <h2 className="text-lg font-medium mb-6 text-center">Validation Results👽</h2>
-        <div>
-          <div
-            className="flex items-center justify-between cursor-pointer"
-            onClick={() => setShowValidEmails(!showValidEmails)}
-          >
-            <h3 className="font-medium text-green-500">
-              Valid Emails ({validEmails.length})
-            </h3>
-            <span className="text-gray-500">{showValidEmails ? "-" : "+"}</span>
+        <h2 className="text-lg font-medium mb-6 mt-2 text-center">Validation Results👽</h2>
+
+        {/* Valid Emails Collapse */}
+        <div className="collapse bg-base-200 mb-4">
+          <input
+            type="checkbox"
+            checked={openCollapse === 0}
+            onChange={() => toggleCollapse(0)}
+          />
+          <div className="collapse-title text-xl font-medium text-green-500">
+            Valid Emails ({validEmails.length})
           </div>
-          {showValidEmails && (
-            <div className="mt-4 space-y-2 max-h-[200px] overflow-y-auto">
-              {validEmails.map((validationResult, index) => (
-                <div key={index} className="border-b border-gray-300 pb-2">
-                  <p>Email: {validationResult.email}</p>
-                  <p className="text-green-500 font-semibold">
-                    Reachable: {validationResult.reachable}
-                  </p>
-                  <p>
-                    Syntax:{" "}
-                    <span className="font-medium">
-                      {JSON.stringify(validationResult.syntax)}
-                    </span>
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-        <div>
-          <div
-            className="flex items-center justify-between cursor-pointer"
-            onClick={() => setShowUnknownEmails(!showUnknownEmails)}
-          >
-            <h3 className="font-medium text-yellow-500">
-              Unknown Emails ({unknownEmails.length})
-            </h3>
-            <span className="text-gray-500">
-              {showUnknownEmails ? "-" : "+"}
-            </span>
+          <div className="collapse-content">
+            {validEmails.length > 0 ? (
+              <div className="space-y-2 max-h-[200px] overflow-y-auto">
+                {validEmails.map((validationResult, index) => (
+                  <div
+                    key={index}
+                    className="border-b border-gray-300 pb-2"
+                  >
+                    <p>Email: {validationResult.email}</p>
+                    <p className="text-green-500 font-semibold">
+                      Reachable: {validationResult.reachable}
+                    </p>
+                    <p>
+                      Syntax:{" "}
+                      <span className="font-medium">
+                        {JSON.stringify(validationResult.syntax)}
+                      </span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className="text-center py-4">No valid emails found.</p>
+            )}
           </div>
-          {showUnknownEmails && (
-            <div className="mt-4 space-y-2 max-h-[200px] overflow-y-auto">
-              {unknownEmails.map((validationResult, index) => (
-                <div
-                  key={index}
-                  className="border-b border-gray-300 dark:border-gray-600 pb-2"
-                >
-                  <p>Email: {validationResult.email}</p>
-                  <p className="text-yellow-500 font-semibold">Reachable: {validationResult.reachable}</p>
-                  <p>
-                    Syntax:{" "}
-                    <span className="font-medium">
-                      {JSON.stringify(validationResult.syntax)}
-                    </span>
-                  </p>
-                  <p className="text-green-500 font-semibold">
-                    Mx Records:{" "}
-                    <span className="font-medium text-green-500">
-                      {JSON.stringify(validationResult.has_mx_records)}
-                    </span>
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
         </div>
-        <div>
-          <div
-            className="flex items-center justify-between cursor-pointer"
-            onClick={() => setShowInvalidEmails(!showInvalidEmails)}
-          >
-            <h3 className="font-medium text-red-500">
-              Invalid Emails ({invalidEmails.length})
-            </h3>
-            <span className="text-gray-500">
-              {showInvalidEmails ? "-" : "+"}
-            </span>
+
+        {/* Unknown Emails Collapse */}
+        <div className="collapse bg-base-200 mb-4">
+          <input
+            type="checkbox"
+            checked={openCollapse === 1}
+            onChange={() => toggleCollapse(1)}
+          />
+          <div className="collapse-title text-xl font-medium text-yellow-500">
+            Unknown Emails ({unknownEmails.length})
           </div>
-          {showInvalidEmails && (
-            <div className="mt-4 space-y-2 max-h-[200px] overflow-y-auto">
-              {invalidEmails.map((validationResult, index) => (
-                <div
-                  key={index}
-                  className="border-b border-gray-300 dark:border-gray-600 pb-2"
-                >
-                  <p>Email: {validationResult.email}</p>
-                  <p>Reachable: {validationResult.reachable}</p>
-                  <p>
-                    Syntax:{" "}
-                    <span className="font-medium">
-                      {JSON.stringify(validationResult.syntax)}
-                    </span>
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
+          <div className="collapse-content">
+            {unknownEmails.length > 0 ? (
+              <div className="space-y-2 max-h-[200px] overflow-y-auto">
+                {unknownEmails.map((validationResult, index) => (
+                  <div
+                    key={index}
+                    className="border-b border-gray-300 pb-2"
+                  >
+                    <p>Email: {validationResult.email}</p>
+                    <p className="text-yellow-500 font-semibold">
+                      Reachable: {validationResult.reachable}
+                    </p>
+                    <p>
+                      Syntax:{" "}
+                      <span className="font-medium">
+                        {JSON.stringify(validationResult.syntax)}
+                      </span>
+                    </p>
+                    <p className="text-green-500 font-semibold">
+                      Mx Records:{" "}
+                      <span className="font-medium text-green-500">
+                        {JSON.stringify(validationResult.has_mx_records)}
+                      </span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className="text-center py-4">No unknown emails found.</p>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="text-sm font-semibold flex justify-start mt-4">
-        <p>
-          Note: <span className="text-yellow-500">unknown</span> means that the
-          server does not allow real-time verification of an email right now, or
-          the email provider is a{" "}
-          <span className="text-red-500">catch-all</span> email server.
-        </p>
+
+        {/* Invalid Emails Collapse */}
+        <div className="collapse bg-base-200 mb-4">
+          <input
+            type="checkbox"
+            checked={openCollapse === 2}
+            onChange={() => toggleCollapse(2)}
+          />
+          <div className="collapse-title text-xl font-medium text-red-500">
+            Invalid Emails ({invalidEmails.length})
+          </div>
+          <div className="collapse-content">
+            {invalidEmails.length > 0 ? (
+              <div className="space-y-2 max-h-[200px] overflow-y-auto">
+                {invalidEmails.map((validationResult, index) => (
+                  <div
+                    key={index}
+                    className="border-b border-gray-300 pb-2"
+                  >
+                    <p>Email: {validationResult.email}</p>
+                    <p>Reachable: {validationResult.reachable}</p>
+                    <p>
+                      Syntax:{" "}
+                      <span className="font-medium">
+                        {JSON.stringify(validationResult.syntax)}
+                      </span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className="text-center py-4">No invalid emails found.</p>
+            )}
+          </div>
+        </div>
+
+        <div className="text-sm font-semibold flex justify-start mt-4 p-2">
+          <p>
+            Note: <span className="text-yellow-500">unknown</span> means that the
+            server does not allow real-time verification of an email right now, or
+            the email provider is a{" "}
+            <span className="text-red-500">catch-all</span> email server.
+          </p>
+        </div>
       </div>
     </div>
   );
